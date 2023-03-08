6d2878c10c68d4c4144e6d0beec5c3567fbe09da
StealthyAD <125747450+StealthyAD@users.noreply.github.com>
2023-03-08 15:35:42 +0100

    Update FearScriptAD.lua

--- FearScriptAD.lua
+++ FearScriptAD.lua
@@ -26,7 +26,7 @@
     local FearVersion = "0.29"
     local FearScriptNotif = "> FearScript Advanced "..FearVersion
     local FearScriptV1 = "FearScript Advanced "..FearVersion
-    local FearSEdition = 100.5
+    local FearSEdition = 100.6
     local FearToast = util.toast
 
     local aalib = require("aalib")
