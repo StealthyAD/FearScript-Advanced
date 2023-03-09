fd79f64a198e100b66fe79bf337af704260c3c7d
StealthyAD <125747450+StealthyAD@users.noreply.github.com>
2023-03-09 22:19:54 +0100

    Add files via upload

---
+++ lib/FearScriptAD/Changelog.lua
@@ -0,0 +1,30 @@
+--[[
+    FearScript Advanced for Stand by StealthyAD.
+    The All-In-One Script combines every each script.
+
+    -- Changelog Script
+
+    Features:
+    - Compatible All Stand Versions.
+    - Includes Standify & Cruise Missile Tool (GitHub)
+
+    Help with Lua?
+    - GTAV Natives: https://nativedb.dotindustries.dev/natives/
+    - FiveM Docs Natives: https://docs.fivem.net/natives/
+    - Stand Lua Documentation: https://stand.gg/help/lua-api-documentation
+    - Lua Documentation: https://www.lua.org/docs.html
+]]--
+
+    local FearToast = util.toast
+    local FearRoot = menu.my_root()
+    local FearHelp = util.show_corner_help
+    local FearVersion = "0.29.8 "
+    local FearHelpNotification = "FearScript Advanced "..FearVersion
+
+    local FearChangelog = FearRoot:list("Changelog Update")
+
+        FearChangelog:divider("FearScript Changelog "..FearVersion)
+
+        FearChangelog:action("Patch 0.29.8 (Stand 100.6)", {}, "", function() -- 0.29.8
+            FearHelp(FearHelpNotification.."\nWhat's new?\n\n - Adding Changelog Update which you can track what's updated.")
+        end)
