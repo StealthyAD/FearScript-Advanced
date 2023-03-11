ede9c21f60f14428f8461e79867ce261bfa1a291
StealthyAD <125747450+StealthyAD@users.noreply.github.com>
2023-03-11 18:45:15 +0100

    Update FearScriptAD.lua

--- FearScriptAD.lua
+++ FearScriptAD.lua
@@ -23,7 +23,7 @@
     util.require_natives(1663599433)
 
     local FearRoot = menu.my_root()
-    local FearVersion = "0.30.6"
+    local FearVersion = "0.30.7"
     local FearScriptNotif = "> FearScript Advanced "..FearVersion
     local FearScriptV1 = "FearScript Advanced "..FearVersion
     local FearSEdition = 100.7
@@ -1130,10 +1130,25 @@
                     end
                 end)
 
-                FearWeapons:toggle_loop("Infinite Ammo", {"ffclip"}, "Lock your ammo to get not reloading fire.\n\nAlternative to Stand, has reloading fire. Better alternative to avoid reloading and made reloading easier without losing time.", function()
-                    WEAPON.SET_PED_INFINITE_AMMO_CLIP(PLAYER.PLAYER_PED_ID(), true)
-                end, function()
-                    WEAPON.SET_PED_INFINITE_AMMO_CLIP(PLAYER.PLAYER_PED_ID(), false)
+                FearWeapons:toggle_loop("Explosive Ammo", {}, "Simple Explosive Ammo for all weapons.", function()
+                    MISC.SET_EXPLOSIVE_AMMO_THIS_FRAME(players.user())
+                end)
+
+                FearWeapons:toggle("Infinite Ammo", {"ffclip"}, "Lock your ammo to get not reloading fire.\n\nAlternative to Stand, has reloading fire. Better alternative to avoid reloading and made reloading easier without losing time.", function(toggle)
+                    local WeaponHashes = { -- Added Hash Weapons to look the real infinite weapon without reloading
+                        0x1B06D571,0xBFE256D4,0x5EF9FEC4,0x22D8FE39,0x3656C8C1,0x99AEEB3B,0xBFD21232,0x88374054,0xD205520E,0x83839C4,0x47757124,
+                        0xDC4DB296,0xC1B3C3D1,0xCB96392F,0x97EA20B8,0xAF3696A1,0x2B5EF5EC,0x917F6C8C,0x13532244,0x2BE6766B,0x78A97CD0,0xEFE7E2DF,
+                        0xA3D4D34,0xDB1AA450,0xBD248B55,0x476BF155,0x1D073A89,0x555AF99A,0x7846A318,0xE284C527,0x9D61E50F,0xA89CB99E,0x3AABBBAA,
+                        0xEF951FBB,0x12E82D3D,0xBFEFFF6D,0x394F415C,0x83BF0278,0xFAD1F1C9,0xAF113F99,0xC0A3098D,0x969C3D67,0x7F229F94,0x84D6FAFD,
+                        0x624FE830,0x9D07F764,0x7FD62962,0xDBBD7280,0x61012683,0x5FC3C11,0xC472FE2,0xA914799,0xC734385A,0x6A6C02E0,0xB1CA77B1,
+                        0xA284510B,0x4DD2DC56,0x42BF8A85,0x7F7497E5,0x6D544C99,0x63AB0442,0x781FE4A,0xB62D1F67,0x93E220BD,0xA0973D5E,0xFDBC8A50,
+                        0x497FACC3,0x24B17070,0x2C3731D9,0xAB564B93,0x787F0BB,0xBA45E8B8,0x23C9F95C
+                    }
+        
+                    for k,v in WeaponHashes do
+                        WEAPON.SET_PED_INFINITE_AMMO(players.user_ped(), toggle, v)
+                        WEAPON.SET_PED_INFINITE_AMMO_CLIP(PLAYER.PLAYER_PED_ID(), toggle)
+                    end
                 end)
 
                 FearWeapons:toggle_loop("Quick Reload", {}, "Reload faster than normal weapon.\n\nRecommended for big magazine which it's very slow to reload", function()
@@ -1153,13 +1168,6 @@
                     not PED.IS_PED_SHOOTING(PLAYER.PLAYER_PED_ID()) then
                     FearTime(900)
                     WEAPON.REFILL_AMMO_INSTANTLY(PLAYER.PLAYER_PED_ID())
-                    end
-                end)
-
-                FearWeapons:toggle("Lock-On Bypass", {}, "", function()
-                    local PlayerTargetPED = players.user_ped()
-                    if WEAPON.GET_SELECTED_PED_WEAPON(PLAYER.PLAYER_PED_ID()) == 1672152130 then
-                        PLAYER.SET_PLAYER_LOCKON_RANGE_OVERRIDE(Fear.PlayerTargetPED, 99999)
                     end
                 end)
 
